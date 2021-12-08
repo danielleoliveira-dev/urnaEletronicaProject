ea2596d6c93c5b58bba9b49b8521623f82dc4476
Danielle Oliveira <danioliv.pro@gmail.com>
2021-12-08 11:25:05 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Big Game Survey 
+[![NPM](https://img.shields.io/npm/l/react)](https://github.com/danielleoliveira-dev/urnaEletronicaProject/blob/main/LICENSE) 
+
+# Sobre o projeto
+
+Urna Eletrônica é um projeto desenvolvido em Javascript, totalmente customizavel.
+
+Este projeto consiste em simular o momento de votação eleitoral em uma urna eletrônica, seguindo os mesmos passos de uma votação real. 
+
+## Layout web
+
+
+
+# Tecnologias utilizadas
+
+## Front end
+- HTML / CSS / JS 
+
+# Autor
+
+Danielle de Oliveira Nogueira
+
+https://www.linkedin.com/in/danielleoliveira-dev/
+
+
+
+
+
---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas=[
+    {
+        titulo: 'VEREADOR',
+        numeros:5,
+        candidatos:[
+            {
+                numero: '38111',
+                nome: 'Fulado de Tal',
+                partido:"ABC",
+                fotos:[
+                    {url:"candidato1.png", legenda: 'Vereador', small:false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido:"DEFG",
+                fotos:[
+                    {url:"candidato2.png", legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido:"ABC",
+                vice:'Cic',
+                fotos:[
+                {url:"candidato4.png", legenda: 'Prefeito'},
+                {url:"candidato5.png", legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulnao',
+                partido:"QWERTY",
+                vice: "Zul",
+                fotos:[
+                {url:"candidato1.png", legenda: 'Prefeito'},
+                {url:"candidato5.png", legenda: 'Vice-Prefeito', small:true}
+                ]
+            },     
+        ]
+    }
+]
